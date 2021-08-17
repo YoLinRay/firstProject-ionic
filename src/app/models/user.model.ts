1379de52afad43b33687ab40187ab33647ea5fd6
ray Lin <r0923835@gmail.com>
2021-08-17 16:49:15 +0800

    shareBoard功能&CSS樣式 修改

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,16 +1,11 @@
 export interface User {
 
     id?: number;
-
     userName?: string;
-
     Email?: string;
-
     password?: string;
-
-    vaildUser?: boolean
-    
-    imageSrc?:string,
-    createdAt?:number,
-    msg?:string
+    vaildUser?: boolean;
+    imageSrc?: string;
+    createdAt?: number;
+    msg?: string;
 }
--- src/app/tab2/tokyo-attractions/act-detail/shareboard/shareboard.page.ts
+++ src/app/tab2/tokyo-attractions/act-detail/shareboard/shareboard.page.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { attractions } from 'src/app/models/Attractions';
 import { AttractionService } from 'src/app/service/attraction.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { USERS } from 'src/app/service/users.mock';
+import { IonContent } from '@ionic/angular';
+import { UserService } from 'src/app/service/user.service';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from 'src/app/components/popover/popover.component';
 @Component({
   selector: 'app-shareboard',
   templateUrl: './shareboard.page.html',
@@ -12,20 +16,40 @@ import { USERS } from 'src/app/service/users.mock';
 export class ShareboardPage implements OnInit {
   attractionOne?: attractions;
   users: User[] = USERS;
-  currentUser = "王小明";
+  loginUser: User[] = [{ userName: "user1", createdAt: 1629179193825, msg: "測試", vaildUser: false }, { userName: "user2", createdAt: 1629179193825, msg: "測試", vaildUser: false }];
   newMsg: string;
-  constructor(private attractionService: AttractionService, private route: ActivatedRoute) { }
+  @ViewChild(IonContent) content: IonContent;   //獲取IonContent
+  constructor(private attractionService: AttractionService, private route: ActivatedRoute, private userService: UserService, private popCtrl: PopoverController) { }
 
   ngOnInit() {
     const attractionId = Number(this.route.snapshot.params.id);
     this.attractionOne = this.attractionService.getAttraction(attractionId);
   }
+  async openPopover(ev: any) {
+    if (this.userService.vaildUser == false) {
+      const popover = await this.popCtrl.create({
+        component: PopoverComponent,
+        event: ev
+      })
+      return await popover.present()
+    }
+  }
   sendMsg() {
-    this.users.push({
-      userName: "王小明",
-      createdAt: new Date().getTime(),
-      msg: this.newMsg
-    });
-    this.newMsg = ''
+    for (let i = 0; i <= this.users.length; i++) {
+      if (this.users[i].vaildUser == true) {
+        this.loginUser.push({
+          userName: this.users[i].userName,
+          createdAt: new Date().getTime(),
+          msg: this.newMsg,
+          vaildUser: true
+        });
+        this.newMsg = "";
+        setTimeout(() => {
+          this.content.scrollToBottom(200);
+        });
+
+      }
+    }
+
   }
 }
